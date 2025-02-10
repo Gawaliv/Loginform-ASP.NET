abd80f43440198489597beb90f8e57da790fb47e
Gawaliv <gawalivaishnavi39@gmail.com>
2025-02-10 23:21:02 +0530

    Add files via upload

---
+++ LoginFormASP.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginFormASP
+{
+    public partial class LoginFormASP : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        
+        {
+            string username = txtUser.Text;
+            string password = txtPass.Text;
+            if (username == "admin" && password == "admin")
+            {
+                Response.Write("<script type='text/javascript/javascript'>alert('Login Successful')</script>");
+            }
+            else
+            {
+                Response.Write("<script type='text/javascript/javascript'>alert('Login Failed')</script>");
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
